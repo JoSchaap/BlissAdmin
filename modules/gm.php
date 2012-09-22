7f43cc5c2c50e4595c42d68eb595be73e563017c
ayan4m1 <andrew@bulletlogic.com>
2012-09-22 18:54:20 -0400

    added switch for multiple marker types

--- modules/gm.php
+++ modules/gm.php
@@ -95,7 +95,7 @@ if (isset($_SESSION['user_id']))
 	};
 	
 	function pollMarkers() {
-	$.getJSON('positions.php', function(markers) {
+	$.getJSON('positions.php?type=vehicles', function(markers) {
 		var map = ChernoMap._map;
 		map.clearMarkers();
 		for (i = 0; i < markers.length; i++) { 
--- positions.php
+++ positions.php
@@ -6,7 +6,22 @@ mysql_connect($hostname, $username, $password) or die (mysql_error());
 mysql_select_db($dbName) or die (mysql_error());
 
 if (isset($_SESSION['user_id'])) {
-	$result = mysql_query("select id, otype, type, pos, instance from objects join object_classes on objects.otype = object_classes.classname");
+	if (!isset($_GET['type'])) {
+		die("[]");
+	}
+
+	switch($_GET['type']) {
+	case 'vehicles':
+		$sql = "select id, otype, type, pos, instance from objects o join object_classes oc on o.otype = oc.classname where o.instance = " . $iid;
+		break;
+	case 'players':
+		$sql = "select s.id, CONCAT('Player ', p.name), 'Player' as type, s.pos as pos, '" . $iid . "' as instance from profile p join survivor s on p.unique_id = s.unique_id where s.is_dead = 0";
+		break;
+	default:
+		die("[]");
+	}	
+	
+	$result = mysql_query($sql);
 	$output = array();
 	for ($i = 0; $i < mysql_num_rows($result); $i++) {
 		$row = mysql_fetch_assoc($result);
