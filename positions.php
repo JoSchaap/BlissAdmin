0e816ca9d529bcb843e4db41939648842df5d481
ayan4m1 <andrew@bulletlogic.com>
2012-09-27 12:51:39 -0400

    add multiple queries to positions.php

--- positions.php
+++ positions.php
@@ -11,11 +11,29 @@ if (isset($_SESSION['user_id'])) {
 	}
 
 	switch($_GET['type']) {
-	case 'vehicles':
+	case 0:
+		$sql = "select s.id, CONCAT('Player ', p.name), 'Player' as type, s.pos as pos, '" . $iid . "' as instance from profile p join survivor s on p.unique_id = s.unique_id where s.is_dead = 0 and last_update > now() - interval 4 hour";
+		break;
+	case 1:
+		$sql = "select s.id, CONCAT('Player ', p.name), 'Player' as type, s.pos as pos, '" . $iid . "' as instance from profile p join survivor s on p.unique_id = s.unique_id where s.is_dead = 0";
+		break;
+	case 2:
+		$sql = "select s.id, concat('Player ', p.name), 'Player' as type, s.pos as pos, '" . $iid ."' from profile p join survivor s on p.unique_id = s.unique_id where s.is_dead = 1";
+		break;
+	case 3:
+		$sql = "select s.id, concat('Player ', p.name), 'Player' as type, s.pos as pos, '" . $iid ."' from profile p join survivor s on p.unique_id = s.unique_id";
+		break;
+	case 4:
 		$sql = "select id, otype, type, pos, instance from objects o join object_classes oc on o.otype = oc.classname where o.instance = " . $iid;
 		break;
-	case 'players':
-		$sql = "select s.id, CONCAT('Player ', p.name), 'Player' as type, s.pos as pos, '" . $iid . "' as instance from profile p join survivor s on p.unique_id = s.unique_id where s.is_dead = 0";
+	case 5:
+		$sql = "select id, otype, type, pos, '" . $iid . "' as instance from spawns s join object_classes oc on s.otype = oc.classname";
+		break;
+	case 6:
+		$sql = "select id, otype, type, pos, instance from objects o join object_classes oc on o.otype = oc.classname where o.otype = 'TentStorage' and o.instance = " . $iid;
+		break;
+	case 7:
+		$sql = "select id, otype, type, pos, instance from objects o join object_classes oc on o.otype = oc.classname where o.otype in ('TentStorage', 'Sandbag1_DZ', 'TrapBear', 'Hedgehog_DZ', 'Wire_cat1') and o.instance = " . $iid;
 		break;
 	default:
 		die("[]");
